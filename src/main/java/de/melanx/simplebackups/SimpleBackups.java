85d529d07455fb9bb7adb6db4d900d2c43745d7d
MelanX <info@melanx.de>
2022-02-27 15:25:25 +0100

    fix display test when server has the mod and client doesn't

--- src/main/java/de/melanx/simplebackups/SimpleBackups.java
+++ src/main/java/de/melanx/simplebackups/SimpleBackups.java
@@ -1,9 +1,13 @@
 package de.melanx.simplebackups;
 
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.network.NetworkConstants;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -18,9 +22,14 @@ public class SimpleBackups {
         network = new SimpleNetwork();
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, ConfigHandler.COMMON_CONFIG);
         MinecraftForge.EVENT_BUS.register(new EventListener());
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
     }
 
     public static SimpleNetwork getNetwork() {
         return network;
+    }
+
+    private void setup(FMLCommonSetupEvent event) {
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (remote, isServer) -> true));
     }
 }
